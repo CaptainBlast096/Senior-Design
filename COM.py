a89a65b5bd31ca3a68aa09f411fd4844d41b857a
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2024-01-27 21:34:29 -0500

    Update COM.py
    Added simplistic data structures and logic to store values. Will debug soon.

--- COM.py
+++ COM.py
@@ -1,24 +1,57 @@
 import serial
 import time
 import logging
+
+#Used for finding the avilable COM port
+    # import serial.tools.list_ports
+
+    # List available ports
+    #ports = list(serial.tools.list_ports.comports())
+    #for p in ports:
+        #print(f"Available port: {p.name}")
+
 # Use 'python -m serial.tools.list_ports' to find port connection
     # Found port should have 'serial' in the name
-    
-serialConnection = serial.Serial(port='insert port name in here',baudrate=9600 )
+Attitude = []
+Draught = []
+Fuel = []
+Heading = []
+Position = []
+XYZ = []
+      
+serialConnection = serial.Serial(port='COM3',baudrate=9600 )
     # baudrate is the rate at which the number of singla elements occurs per second
         # Can modify in the Raspberry Pi and have it match here
         # Default should be set to 9600
         
 while True: # Loop is running for infinity adjust it how you see fit
-    value = serialConnection.readline()
-    valueInString = str(value, 'UTF-8')
-    print(valueInString)
-        # Will convert the output to a string and print
-        
+    try:
+        value = serialConnection.readline()
+        valueInString = str(value, 'UTF-8')
+        if valueInString:
+            print(valueInString)
+            # Will convert the output to a string and print
+            #Send messages to arrays 
+        if valueInString.startswith('$PIHC'):
+            Draught.append(valueInString)
+            
+        elif valueInString.startswith('$PPDS'):
+            Fuel.append(valueInString)
+            
+        elif valueInString.startswith('$ECHDT'):
+            Heading.append(valueInString)
+            
+        elif valueInString.startswith('$GPGGA'):
+            Position.append(valueInString)
+            
+        elif valueInString.startswith('$GLTOP'):
+            XYZ.append(valueInString)
+            
+        elif valueInString.startswith('$PHTRO'):
+            Attitude.append(valueInString)
+        # 1st test to see if contents are stored in respective arrays 2nd Check if any data will be leaked 3rd The limitations of contact data being stored in arrays    
+    except Exception as e:
+        print(f"Error: {e}")
 # serialConnection.close()
     # Will stop the connection 
-    
-    
-# Will try to implement a data structure to hold the information
-      # Also add error handling and use the time library for delays 
-      # Maybe will use logging library for sending flags
+
